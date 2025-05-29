748565cc73d45c6f680c882498c21c9a449fd825
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2025-05-30 02:34:06 +0900

    Add tooltip prop to ClickableIcon

--- src/components/ClickableIcon.tsx
+++ src/components/ClickableIcon.tsx
@@ -2,14 +2,26 @@ import type { IconName } from 'obsidian';
 import { splitProps } from 'solid-js';
 import type { JSX, ParentComponent } from 'solid-js';
 import Icon from './Icon';
+import type { TooltipConfig } from 'src/directives';
+import { tooltip } from 'src/directives';
+
+void tooltip;
 
 const ClickableIcon: ParentComponent<
     JSX.HTMLAttributes<HTMLDivElement> & {
         id: IconName;
         isActive?: boolean;
+        tooltip?: TooltipConfig;
     }
 > = props => {
-    const [local, others] = splitProps(props, ['id', 'isActive', 'children', 'classList', 'class']);
+    const [local, others] = splitProps(props, [
+        'id',
+        'isActive',
+        'children',
+        'classList',
+        'class',
+        'tooltip',
+    ]);
     return (
         <div
             class={local.class}
@@ -18,6 +30,7 @@ const ClickableIcon: ParentComponent<
                 'is-active': local.isActive,
                 ...local.classList,
             }}
+            use:tooltip={local.tooltip}
             {...others}
         >
             <Icon id={local.id} />
