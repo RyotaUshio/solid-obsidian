73ff1955a2cc844339b1bfa3508dde39f0c153dd
RyotaUshio <72342591+RyotaUshio@users.noreply.github.com>
2025-07-20 03:06:48 +0000

    Format

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,16 +1,18 @@
 import { type Instruction } from 'obsidian';
 import { For, splitProps, type JSX } from 'solid-js';
 
-export default function Instructions(props: JSX.HTMLAttributes<HTMLDivElement> & {
-    instructions: Instruction[];
-}) {
+export default function Instructions(
+    props: JSX.HTMLAttributes<HTMLDivElement> & {
+        instructions: Instruction[];
+    },
+) {
     const [local, others] = splitProps(props, ['instructions']);
     return (
-        <div class='prompt-instructions' {...others}>
+        <div class="prompt-instructions" {...others}>
             <For each={local.instructions}>
                 {instruction => (
-                    <div class='prompt-instruction'>
-                        <span class='prompt-instruction-command'>{instruction.command}</span>
+                    <div class="prompt-instruction">
+                        <span class="prompt-instruction-command">{instruction.command}</span>
                         <span>{instruction.purpose}</span>
                     </div>
                 )}
--- src/components/RenderResults.tsx
+++ src/components/RenderResults.tsx
@@ -1,15 +1,20 @@
 import { renderResults, type SearchResult } from 'obsidian';
 import { type JSX, splitProps } from 'solid-js';
 
-export default function RenderResults(props: JSX.HTMLAttributes<HTMLDivElement> & {
-    text: string;
-    result: SearchResult;
-    offset?: number;
-}) {
+export default function RenderResults(
+    props: JSX.HTMLAttributes<HTMLDivElement> & {
+        text: string;
+        result: SearchResult;
+        offset?: number;
+    },
+) {
     const [local, others] = splitProps(props, ['text', 'result', 'offset']);
     return (
-        <div {...others} ref={el => {
-            renderResults(el, local.text, local.result, local.offset);
-        }} />
+        <div
+            {...others}
+            ref={el => {
+                renderResults(el, local.text, local.result, local.offset);
+            }}
+        />
     );
 }
